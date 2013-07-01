78d6e8fb4f301eb708b7e1a1890913402e60bcc5
Brian Willard <bwillard+github@gmail.com>
2013-06-30 22:42:39 -0500

    Add support for e-mails via Mandrill

---
+++ BuiltInAdapters/Notification/MandrillAdapter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Twilio;
+using HomeIntegrationPlatform.Engine.Adapters;
+using HomeIntegrationPlatform.Engine;
+using MailChimp;
+
+namespace HomeIntegrationPlatform.BuiltInAdapters.Notification
+{
+    class MandrillAdapter : AdapterBase, INotifyingAdapter
+    {
+        private static AdapterConfiguration configuration = new AdapterConfiguration(new AdaperConfigurationValue[] {
+            new AdaperConfigurationValue("MandrillApiKey"),
+            new AdaperConfigurationValue("MandrillFromAddress"),
+        });
+
+        private readonly MandrillApi client;
+        private readonly string fromAddress;
+
+        public MandrillAdapter(Settings settings)
+            : base(configuration, settings)
+        {
+            client = new MandrillApi(configuration.GetValue("MandrillApiKey"));
+            this.fromAddress = configuration.GetValue("MandrillFromAddress");
+        }
+
+        public void Notify(string toNumber, string body)
+        {
+            var message = new MailChimp.Types.Mandrill.Messages.Message();
+            message.Subject = "Message from Home Automation Platform";
+            message.Text = body;
+            message.FromEmail = this.fromAddress;
+            this.client.Send(message);
+        }
+    }
+}
